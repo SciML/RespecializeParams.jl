ad029a033ea97decd2a442381a7b0e7a3f2a2a0d
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-20 09:44:46 -0400

    Use default public API QA
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- test/qa/qa.jl
+++ test/qa/qa.jl
@@ -18,9 +18,7 @@ using Test
 #    without hiding any real non-public access.
 run_qa(
     RespecializeParams;
-    api_docs_kwargs = (; rendered = true),
     jet_kwargs = (; target_modules = (RespecializeParams,)),
-    explicit_imports = true,
     ei_kwargs = (;
         all_qualified_accesses_are_public = (;
             ignore = (:RefValue, Symbol("@preserve"), :unsafe_convert),
